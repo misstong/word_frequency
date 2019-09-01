7d29e0ee460a11fd9c507b41de1b07c54a8fc694
misstong <1962788480@qq.com>
2019-09-01 09:10:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     CSRT_ENABLED = True
     SECRTE_KEY = ''
     SQLALCHEMY_TRACK_MODIFICATION = False
-    SQLALCHEMY_USER = 'postgres'
-    SQLALCHEMY_PASSWORD = 'tang'
+    SQLALCHEMY_USER = 'xxx'
+    SQLALCHEMY_PASSWORD = 'xxx'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://%s:%s@localhost/wordcount_dev" % (SQLALCHEMY_USER,SQLALCHEMY_PASSWORD)
     
 class ProductionConfig(Config):
